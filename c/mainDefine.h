8196af770cb96232392c730e371525c2228b456f
Daeraa <124400526+Daeraa@users.noreply.github.com>
2023-02-03 21:37:51 +0100

    Add files via upload

---
+++ c/mainDefine.h
@@ -0,0 +1,23 @@
+#ifndef MAINDEFINE
+#define MAINDEFINE
+
+/* std library */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/* optional library */
+#include <stdbool.h>
+#include <string.h>
+#include <errno.h>
+#include <math.h>
+// #include <time.h>
+
+#include "./List/ListBaseFunction.h"
+#include "./List/ListCalculus.h"
+#include "./List/PreSortList.h"
+#include "./List/ListSort.h"
+#include "./List/ListFile.h"
+#include "./List/ListOptions.h"
+
+#endif
